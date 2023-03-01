963374c7b322aac651af51287779ab48b919f2d5
annasohromova <120181353+annasohromova@users.noreply.github.com>
2023-03-01 14:58:05 +0200

    Add files via upload

--- module1.py
+++ module1.py
@@ -1,3 +1,6 @@
+
+
+
 def failist_to_dict(f:str):
     riik_pealinn={}
     pealinn_riik={}
@@ -8,3 +11,23 @@ def failist_to_dict(f:str):
         pealinn_riik[v]=k 
     file.close() 
     return riik_pealinn,pealinn_riik 
+
+def leidma(k:dict, v:int):
+    word=input()
+    x=riigid_pealinnad.get(word)
+    if x==None:
+        p=riigid_pealinnad.get(word)
+        if p==None:
+            a=input('Lisada uue riigi või pealinn? ->').lower()
+            while a not in ['jah','ei']:
+                a=input('Ainult jah või ei')
+            if a=='jah':
+                loc=input(f'Kas {word} on riigi või pealinn? ').lower() 
+
+
+def paranda():
+
+    for line in file 
+
+
+            
